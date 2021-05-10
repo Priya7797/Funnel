03f86026c44394aa62120e36e4867ea5c02e01f9
Priya Singh <priya7797singh@gmail.com>
2021-05-10 23:21:27 +0530

    c

--- funnelChartedit.js
+++ funnelChartedit.js
@@ -238,16 +238,16 @@ chart.legend.position = "left";
 chart.legend.valign = "bottom";
 chart.legend.margin(5,5,20,5);*/
 
-// if(this._legendValue == false){
-// 	series.ticks.template.disabled = true;
-// 	series.alignLabels = false;
-// 	series.labels.template.relativeRotation = 90;
-// 	series.labels.template.radius = am4core.percent(-70);
-// 	series.labels.template.fill = am4core.color("white");
-// 	series.labels.template.fontSize = 11;
-// 	series.labels.template.maxWidth = 60
+if(this._legendValue == false){
+	series.ticks.template.disabled = true;
+	series.alignLabels = false;
+	series.labels.template.relativeRotation = 90;
+	series.labels.template.radius = am4core.percent(-70);
+	series.labels.template.fill = am4core.color("white");
+	series.labels.template.fontSize = 11;
+	series.labels.template.maxWidth = 60
 	
-// }
+}
 
 //sorted order of funnel chart
 function compare( a, b ) {
