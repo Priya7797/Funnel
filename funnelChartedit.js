b3081769b1bb92926a7e5b9f10921344c3bbac95
Priya Singh <priya7797singh@gmail.com>
2021-05-12 00:38:52 +0530

    M

--- funnelChartedit.js
+++ funnelChartedit.js
@@ -246,7 +246,7 @@ if(this._statusCheckBox == true){
 	console.log("Print Cat");
 	// chart.legend.labels.template.text = "{name}";
 	// series.legendSettings.labelText = "{name}";
-	series.legendSettings.itemValueText = " ";
+	series.legendSettings.itemValueText = null;
 
 	}
 /*chart.legend = new am4charts.Legend();
